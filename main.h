c3b33048261a6b23965753cecd4d58eee9926c78
SatellitePeace <njokunneoma1999@gmail.com>
2022-04-17 21:06:05 +0200

    prototype/header file for printf functions

--- main.h
+++ main.h
@@ -1,22 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _PRINTF_MAIN_H
+#define _PRINTF_MAIN_H
 
-#include <limits.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "stdarg.h"
+#include "stdlib.h"
+#include "stdio.h"
 
-/* Flag Modifier Macros */
-#define PLUS 1
-#define SPACE 2
-#define HASH 4
-#define ZERO 8
-#define NEG 16
-#define PLUS_FLAG (flags & 1)
-#define SPACE_FLAG ((flags >> 1) & 1)
-#define HASH_FLAG ((flags >> 2) & 1)
-#define ZERO_FLAG ((flags >> 3) & 1)
-#define NEG_FLAG ((flags >> 4) & 1)
+int _putchar(char c);
+int _printf(const char *format, ...);
+int put_str(char *str);
+int put_int(long n);
+int get_printing_func(char a, va_list *ap);
+int put_rot13(char *s);
+char *rot13(char *s);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int put_binary(unsigned int n);
 
 /* Length Modifier Macros */
 #define SHORT 1
@@ -96,21 +94,4 @@ int handle_precision(va_list args, const char *modifier, char *index);
 unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
 		unsigned char, int, int, unsigned char);
 
-/* Modifiers */
-unsigned int print_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
-unsigned int print_string_width(buffer_t *output,
-		unsigned char flags, int wid, int prec, int size);
-unsigned int print_neg_width(buffer_t *output, unsigned int printed,
-		unsigned char flags, int wid);
-
-/* Helper Functions */
-buffer_t *init_buffer(void);
-void free_buffer(buffer_t *output);
-unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
-unsigned int convert_sbase(buffer_t *output, long int num, char *base,
-		unsigned char flags, int wid, int prec);
-unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
-		unsigned char flags, int wid, int prec);
-
-#endif
+#endif /* _PRINTF_MAIN_H */
