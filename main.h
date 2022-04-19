2f855e0e3a414ace6801e38edb50ca71d22a5e58
SatellitePeace <njokunneoma1999@gmail.com>
2022-04-19 19:17:52 +0200

    printf header file

--- main.h
+++ main.h
@@ -1,97 +1,97 @@
-#ifndef _PRINTF_MAIN_H
-#define _PRINTF_MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include "stdarg.h"
-#include "stdlib.h"
-#include "stdio.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <stdbool.h>
 
+/* macros */
+#define FLUSH -1
+#define PF_INIT {false, false, false}
+#define NIL "(nil)"
+#define HEXA "0x"
+#define NULL_STRING "(null)"
+#define NUL '\0'
+#define KILOBYTE 1024
+#define ABS(x) (((x) <  0) ? -(x) : (x))
+
+/**
+ * struct modifiers - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct modifiers
+{
+	_Bool plus;
+	_Bool space;
+	_Bool hash;
+} mods;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, mods *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, mods *f);
+void print_number(int n);
+int print_unsigned(va_list l, mods *f);
+int count_digit(int i);
+
+/* print_bases */
+int print_hex(va_list l, mods *f);
+int print_hex_big(va_list l, mods *f);
+int print_binary(va_list l, mods *f);
+int print_octal(va_list l, mods *f);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* _printf */
+int _printf(const char *format, ...);
+
+/* get_print */
+int (*get_print(char s))(va_list, mods *);
+
+/* get_flags */
+int get_flags(char s, mods *f);
+
+/* print_alpha */
+int print_string(va_list l, mods *f);
+int print_char(va_list l, mods *f);
+
+/* write_funcs */
 int _putchar(char c);
-int _printf(const char *format, ...);
-int put_str(char *str);
-int put_int(long n);
-int get_printing_func(char a, va_list *ap);
-int put_rot13(char *s);
-char *rot13(char *s);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-int put_binary(unsigned int n);
+int _puts(char *str);
 
-/* Length Modifier Macros */
-#define SHORT 1
-#define LONG 2
+/* print_custom */
+int print_rot13(va_list l, mods *f);
+int print_rev(va_list l, mods *f);
+int print_bigS(va_list l, mods *f);
+_Bool isNonAlphaNumeric(char c);
 
-/**
- * struct buffer_s - A new type defining a buffer struct.
- * @buffer: A pointer to a character array.
- * @start: A pointer to the start of buffer.
- * @len: The length of the string stored in buffer.
- */
-typedef struct buffer_s
-{
-	char *buffer;
-	char *start;
-	unsigned int len;
-} buffer_t;
+/* print_address */
+int print_address(va_list l, mods *f);
 
-/**
- * struct converter_s - A new type defining a converter struct.
- * @specifier: A character representing a conversion specifier.
- * @func: A pointer to a conversion function corresponding to specifier.
- */
-typedef struct converter_s
-{
-	unsigned char specifier;
-	unsigned int (*func)(va_list, buffer_t *,
-			unsigned char, int, int, unsigned char);
-} converter_t;
+/* print_percent */
+int print_percent(va_list l, mods *f);
 
-/**
- * struct flag_s - A new type defining a flags struct.
- * @flag: A character representing a flag.
- * @value: The integer value of the flag.
- */
-typedef struct flag_s
-{
-	unsigned char flag;
-	unsigned char value;
-} flag_t;
+/* assertions */
+_Bool invalidInputs(const char *p);
 
-int _printf(const char *format, ...);
+/* unit_tests */
+void tusBasicTester(void);
+void tusAdvancedTester(void);
+void tusSuperAdvanced(void);
 
-/* Conversion Specifier Functions */
-unsigned int convert_c(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_s(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_di(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_percent(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_b(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_u(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_o(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_x(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_X(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_S(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_p(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_r(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-unsigned int convert_R(va_list args, buffer_t *output,
-		unsigned char flags, int wid, int prec, unsigned char len);
-
-/* Handlers */
-unsigned char handle_flags(const char *flags, char *index);
-unsigned char handle_length(const char *modifier, char *index);
-int handle_width(va_list args, const char *modifier, char *index);
-int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-		unsigned char, int, int, unsigned char);
-
-#endif /* _PRINTF_MAIN_H */
+#endif
